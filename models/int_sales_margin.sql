4bdb6d0b5872523cdc0afd212c538926f5478c39
mariabatinovich@gmail.com <mariabatinovich@gmail.com>
2025-12-02 19:23:34 +0000

    added purchase cost and margin

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,12 +1,11 @@
-SELECT 
-    s.products_id, 
-    s.date_date, 
-    s.orders_id,
-    s.revenue, 
-    s.quantity, 
-    CAST(purchase_price AS float64) AS purchase_price, 
-    ROUND(s.quantity*CAST(p.purchase_price AS float64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS float64),2) AS margin
-from {{"stg_gz_raw_data__raw_gz_sales"}} as s
-left join {{"stg_gz_raw_data__raw_gz_product"}} as p
+select products_id, 
+    date_date, 
+    orders_id,
+    revenue,
+    quantity,
+    CAST(purchSE_price AS FLOAT64),
+    ROUND(sales.quantity * CAST(product.purchSE_price AS FLOAT64),2) AS purchase_cost,
+    ROUND(sales.revenue - sales.quantity * CAST(product.purchSE_price AS FLOAT64),2) AS margin
+from {{"gz_raw_data.raw_gz_sales"}} as sales
+left join {{"gz_raw_data.raw_gz_product"}} as product
     on sales.pdt_id = product.products_id
