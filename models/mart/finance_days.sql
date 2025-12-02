b01a511142bc4c145f6b617f604b62636ff6bb17
mariabatinovich@gmail.com <mariabatinovich@gmail.com>
2025-12-02 20:33:23 +0000

    configured to save as table

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,29 +1,34 @@
- WITH orders_per_day AS (
-   SELECT
-     date_date
-     ,COUNT(DISTINCT orders_id) AS nb_transactions
-     ,ROUND(SUM(revenue),0) AS revenue
-     ,ROUND(SUM(margin),0) AS margin
-     ,ROUND(SUM(operational_margin),0) AS operational_margin
-     ,ROUND(SUM(purchase_cost),0) AS purchase_cost
-     ,ROUND(SUM(shipping_fee),0) AS shipping_fee
-     ,ROUND(SUM(logcost),0) AS log_cost
-     ,ROUND(SUM(ship_cost),0) AS ship_cost
-     ,SUM(quantity) AS quantity
- FROM {{ref("int_orders_operational")}}
- GROUP BY  date_date
- )
+{{ config(
+    materialized='table'
+) }}
 
- SELECT
-     date_date
-     , revenue
-     , margin
-     , operational_margin
-     , purchase_cost
-     , shipping_fee
-     , log_cost
-     , ship_cost
-     , quantity
-     , ROUND(revenue/NULLIF(nb_transactions, 0), 2) AS average_basket
- FROM orders_per_day
- ORDER BY  date_date DESC
+with
+    orders_per_day as (
+        select
+            date_date,
+            count(distinct orders_id) as nb_transactions,
+            round(sum(revenue), 0) as revenue,
+            round(sum(margin), 0) as margin,
+            round(sum(operational_margin), 0) as operational_margin,
+            round(sum(purchase_cost), 0) as purchase_cost,
+            round(sum(shipping_fee), 0) as shipping_fee,
+            round(sum(logcost), 0) as log_cost,
+            round(sum(ship_cost), 0) as ship_cost,
+            sum(quantity) as quantity
+        from {{ ref("int_orders_operational") }}
+        group by date_date
+    )
+
+select
+    date_date,
+    revenue,
+    margin,
+    operational_margin,
+    purchase_cost,
+    shipping_fee,
+    log_cost,
+    ship_cost,
+    quantity,
+    round(revenue / nullif(nb_transactions, 0), 2) as average_basket
+from orders_per_day
+order by date_date desc
