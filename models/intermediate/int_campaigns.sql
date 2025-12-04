0036935b4f98cfd943f759b13b4d0d5130ff73af
mariabatinovich@gmail.com <mariabatinovich@gmail.com>
2025-12-04 19:56:58 +0000

    fiixed staging and added intermediate table

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,17 @@
+SELECT *
+FROM {{ ref('stg_gz_raw_data__raw_gz_adwords') }}
+
+UNION ALL
+
+SELECT *
+FROM {{ ref('stg_gz_raw_data__raw_gz_bing') }}
+
+UNION ALL
+
+SELECT *
+FROM {{ ref('stg_gz_raw_data__raw_gz_criteo') }}
+
+UNION ALL
+
+SELECT *
+FROM {{ ref('stg_gz_raw_data__raw_gz_facebook') }}
